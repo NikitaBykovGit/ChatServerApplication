5b2201ad4921a5e3750880d7f8d2dd66f1f62557
Nikita <nikita.bykov1995@gmail.com>
2024-04-25 16:38:50 +0300

    Added code

--- ChatApplication/chatserver/consumers.py
+++ ChatApplication/chatserver/consumers.py
@@ -1,14 +1,22 @@
 import json
-from time import sleep
+from datetime import datetime
 
+from django.forms.models import model_to_dict
 from channels.generic.websocket import WebsocketConsumer
+
+from chatserver.models import Message
 
 
 class WSRoomConsumer(WebsocketConsumer):
+    is_activated = None
+
     def connect(self):
         self.accept()
-        for i in range(100):
-            self.send(json.dumps({'message': i}))
-            sleep(1)
-
-
+        self.is_activated = datetime.now()
+        while True:
+            new_message = Message.objects.filter(time__gt=self.is_activated)
+            if new_message.exists():
+                dict_message = model_to_dict(new_message[0])
+                print(json.dumps(dict_message))
+                self.is_activated = datetime.now()
+                self.send(json.dumps(dict_message))
